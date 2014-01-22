c171abf2040b0dda9241901b216c7ef6f722af48
Akshay Mittal <akshay@cs.princeton.edu>
2014-01-22 16:52:29 -0500

    tasks updated

---
+++ public/js/GameScore.js
@@ -0,0 +1,16 @@
+
+var GameScore = function(player) { // later change it to team
+	var
+	player,
+	points,
+	redSets,
+	blackSets;
+
+	var getPoints = function() {
+		return points;
+	};
+
+	var addPoints = function() {
+		
+	}
+}
